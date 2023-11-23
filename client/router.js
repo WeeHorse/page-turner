9b39701a36979f2c9af580dfd588a74c5a38ada0
WeeHorse <benjaminberglund@gmail.com>
2023-11-23 09:59:57 +0100

    added about, contact

--- client/router.js
+++ client/router.js
@@ -3,14 +3,26 @@ import products from "./pages/products.js"
 
 async function router() {
   switch (location.hash) {
+
     case "":
       $('main').html(home())
       break
+
     case "#products":
       $('main').html(await products())
       break
+
+    case "#about":
+      $('main').html(`<h2>About our historical endeavour</h2>`)
+      break
+
+    case "#contact":
+      $('main').html(`<h2>Contact information</h2><p>Is provided on a need to know basis</p>`)
+      break
+
     default:
       $('main').html(`<h2><strong>404</strong> Good job! You've broken the internet.</h2>`)
+
   }
 }
 
