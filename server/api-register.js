d756967980d87be470ffd26e6454c9218efd86ea
WeeHorse <benjaminberglund@gmail.com>
2023-11-23 11:27:56 +0100

    added login api

--- server/api-register.js
+++ server/api-register.js
@@ -1,10 +1,11 @@
 // add REST API:s
 import products from "./apis/products.js"
+import login from "./apis/login.js"
 
 
 export default function(server, db){
-  // connect rest api:s
+  // connect rest api:s to web server and database
   products(server, db)
-  
+  login(server, db)
 
 }
